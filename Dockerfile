7e6d72b4a3d196fb5572383243a7feee40e83acb
Andriy Bulynko <andriy.bulynko@qlik.com>
2020-04-30 20:56:10 -0400

    - more wip

--- Dockerfile
+++ Dockerfile
@@ -1,21 +1,12 @@
 FROM google/cloud-sdk:alpine
 
-RUN apk add --update coreutils
+RUN apk add --update coreutils jq
 
-ENV KUSTOMIZE_VERSION 0.0.24
 ENV KUBECTL_VER 1.17.4
-RUN wget -O /tmp/kustomize.tar.gz https://github.com/qlik-oss/kustomize/releases/download/qlik%2Fv${KUSTOMIZE_VERSION}/kustomize_qlik_v${KUSTOMIZE_VERSION}_linux_amd64.tar.gz
-RUN cd /tmp/ && tar xfv kustomize.tar.gz && chmod +x kustomize && mv kustomize /usr/local/bin/
-
-
 RUN wget -O /usr/local/bin/kubectl https://storage.googleapis.com/kubernetes-release/release/v${KUBECTL_VER}/bin/linux/amd64/kubectl \
     && chmod +x /usr/local/bin/kubectl
 
 COPY --from=mikefarah/yq /usr/bin/yq /usr/local/bin/yq
-
-RUN mkdir -p /src/ejson-keys
-#script will read k8s secrets and put ejson keys into this directory
-ENV EJSON_KEYDIR /src/ejson-keys
 
 COPY run-script.sh /src/
 WORKDIR /src
--- run-script.sh
+++ run-script.sh
@@ -22,11 +22,23 @@ cd ${CONFIG_OUT_DIR}
 git checkout ${BRANCH}
 echo "checked out branch: ${BRANCH}"
 
-tar -zcf ../${CONFIG_OUT_DIR}.tgz .
+echo "compressing config directory in preparation for a kustomization request"
+tar -czf ../${CONFIG_OUT_DIR}.tgz .
 cd ..
 
 CR_BASE64=$(echo "${YAML_CONF}" | base64 -w 0)
 CONFIG_BASE64=$(cat "${CONFIG_OUT_DIR}.tgz" | base64 -w 0)
 echo '{"cr":"'${CR_BASE64}'","config":"'${CONFIG_BASE64}'"}' > post_data.json
-curl -X POST -H "Content-Type: application/json" -d @./post_data.json http://${OPERATOR_SERVICE_NAME}:${OPERATOR_SERVICE_PORT}/kuz
 
+KUZ_SERVICE_ENDPOINT=http://${OPERATOR_SERVICE_NAME}:${OPERATOR_SERVICE_PORT}/kuz
+echo "sending a kustomization request to: ${KUZ_SERVICE_ENDPOINT}"
+KUZ_RESPONSE=$(curl -X POST -H "Content-Type: application/json" -d @./post_data.json http://${KUZ_SERVICE_ENDPOINT}/kuz)
+echo "received a kustomization response"
+
+echo "uncompressing the kustomization response"
+echo ${KUZ_RESPONSE} | jq '.manifests' > manifests.tgz
+mkdir manifests
+tar -xzf manifests.tgz -C ./manifests
+
+echo "applying the kustomized manifest(s) to the cluster"
+kubectl apply -f ./manifests
